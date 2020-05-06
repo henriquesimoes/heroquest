a7b71196fd3356491def032c8e10217897c9cc76
matheus <matheussilva.dd@gmail.com>
2020-05-06 15:08:37 -0300

    Verificando possibilidade de uso de Builder pattern

--- src/br/unicamp/ic/mc322/heroquest/WeaponsAndSpells/Weapon.java
+++ src/br/unicamp/ic/mc322/heroquest/WeaponsAndSpells/Weapon.java
@@ -4,9 +4,87 @@ public class Weapon extends Item {
     private int attackPower;
     private int attackBonus;
     private int attackDistance;
-    private boolean canDiagonallyAttack;
+    private boolean diagonallyAttack;
+    private boolean twoHanded;
 
-    public Weapon(String weaponName, String weaponDescription, int weaponDurability) {
-        super(weaponName, weaponDescription, weaponDurability);
+    public static class Builder {
+        private String weaponName;
+        private String weaponDescription;
+        private int attackPower;
+        private int attackBonus;
+        private int attackDistance;
+        private boolean diagonallyAttack;
+        private boolean twoHanded;
+        private int weaponDurability;
+
+        public Builder() {}
+
+        public Builder setWeaponName(String weaponName) {
+            this.weaponName = weaponName;
+            return this;
+        }
+
+        public Builder setWeaponDescription(String weaponDescription) {
+            this.weaponDescription =  weaponDescription;
+            return this;
+        }
+
+        public Builder setAttackPower(int attackPower) {
+            this.attackPower = attackPower;
+            return this;
+        }
+
+        public Builder setAttackDistance(int attackDistance) {
+            this.attackDistance = attackDistance;
+            return this;
+        }
+
+        public Builder setDiagonallyAttack(boolean diagonallyAttack) {
+            this.diagonallyAttack = diagonallyAttack;
+            return this;
+        }
+
+        public Builder setAsTwoHandedWeapon(boolean twoHanded) {
+            this.twoHanded = twoHanded;
+            return this;
+        }
+
+        public Builder setWeaponDurability(int weaponDurability) {
+            this.weaponDurability = weaponDurability;
+            return this;
+        }
+
+        public Weapon build() {
+            return new Weapon(this);
+        }
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public int getAttackBonus() {
+        return attackBonus;
+    }
+
+    public int getAttackDistance() {
+        return attackDistance;
+    }
+
+    public boolean canDiagonallyAttack() {
+        return diagonallyAttack;
+    }
+
+    public boolean isTwoHanded() {
+        return twoHanded;
+    }
+
+    private Weapon(Builder builder) {
+        super(builder.weaponName, builder.weaponDescription, builder.weaponDurability);
+        this.attackPower = builder.attackPower;
+        this.attackBonus = builder.attackBonus;
+        this.attackDistance = builder.attackDistance;
+        this.diagonallyAttack = builder.diagonallyAttack;
+        this.twoHanded = builder.twoHanded;
     }
 }
