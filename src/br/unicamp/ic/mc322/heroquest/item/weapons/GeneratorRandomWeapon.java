a8efcf23eaac6934ac46b92555f0b5ead8c054ae
Henrique Simões <henriquesimoespa@gmail.com>
2020-06-17 00:01:25 -0300

    Refactor weapon random generator

--- src/br/unicamp/ic/mc322/heroquest/item/weapons/GeneratorRandomWeapon.java
+++ src/br/unicamp/ic/mc322/heroquest/item/weapons/GeneratorRandomWeapon.java
@@ -6,37 +6,20 @@ import java.util.Random;
 
 public class GeneratorRandomWeapon {
     public static Weapon getRandomWeapon() {
-        Weapon weapon;
-        int size = 7;
         Random rand = new Random();
-        int choice = rand.nextInt(size);
 
-        switch (choice){
-            case 0:
-                weapon = new BattleAxe();
-                break;
-            case 1:
-                weapon = new Crossbow();
-                break;
-            case 2:
-                weapon = new Dagger();
-                break;
-            case 3:
-                weapon = new Flail();
-                break;
-            case 4:
-                weapon = new LongSword();
-                break;
-            case 5:
-                weapon = new ShortSword();
-                break;
-            case 6:
-                weapon = new Staff();
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + choice);
-        }
+        Weapon[] possibleWeapons = {
+                new BattleAxe(),
+                new Crossbow(),
+                new Dagger(),
+                new Flail(),
+                new LongSword(),
+                new ShortSword(),
+                new Staff(),
+        };
 
-        return weapon;
+        int choice = rand.nextInt(possibleWeapons.length);
+
+        return possibleWeapons[choice];
     }
 }
