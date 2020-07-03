3d0d1a8aebec122b3d60f381d3d12fee5f907984
Renan Fernando <renanfernando@localhost.localdomain>
2020-07-03 20:58:20 -0300

    Refactor in Randomizer

--- src/br/unicamp/ic/mc322/heroquest/util/randomizer/Randomizer.java
+++ src/br/unicamp/ic/mc322/heroquest/util/randomizer/Randomizer.java
@@ -5,16 +5,23 @@ import java.util.Random;
 public class Randomizer {
     private static Random random;
 
-    public static int randInt(int min, int max){
+    private static void create(){
         if(random == null)
             random = new Random();
+    }
+
+    /**
+     * @return an integer in range [min, max]
+     */
+    public static int randInt(int min, int max){
         return random.nextInt(max - min + 1) + min;
     }
 
-
+    /**
+     * @return an integer in range [0, max)
+     */
     public static int nextInt(int max){
-        if(random == null)
-            random = new Random();
+        create();
         return random.nextInt(max);
     }
 }
