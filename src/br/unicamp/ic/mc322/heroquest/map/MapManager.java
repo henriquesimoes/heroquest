ea1106ab322fde2043461e9aa02b601228daaa84
Henrique Simões <henriquesimoespa@gmail.com>
2020-07-11 20:33:13 -0300

    Add monster generator

--- src/br/unicamp/ic/mc322/heroquest/map/MapManager.java
+++ src/br/unicamp/ic/mc322/heroquest/map/MapManager.java
@@ -5,12 +5,17 @@ import br.unicamp.ic.mc322.heroquest.map.core.MapBuilder;
 import br.unicamp.ic.mc322.heroquest.map.generator.MapGenerator;
 import br.unicamp.ic.mc322.heroquest.map.loader.CorruptedConfigurationFileException;
 import br.unicamp.ic.mc322.heroquest.map.loader.MapLoader;
+import br.unicamp.ic.mc322.heroquest.walker.MonsterGenerator;
+import br.unicamp.ic.mc322.heroquest.walker.Walker;
 
 import java.io.FileNotFoundException;
 
 public class MapManager {
     private MapLoader loader;
     private MapGenerator generator;
+
+    private static final int NUMBER_OF_MONSTERS = 20;
+    private static final int MINIMUM_MONSTER_PER_TYPE = 1;
 
     public MapManager() {
         loader = new MapLoader();
@@ -20,26 +25,40 @@ public class MapManager {
     public Map load(String name) throws FileNotFoundException, CorruptedConfigurationFileException {
         MapBuilder builder = loader.load(name);
 
-        populate(builder);
-
-        return builder.getResult();
+        return populate(builder);
     }
 
     public Map generate() {
         MapBuilder builder = generator.generate();
 
-        populate(builder);
-
-        return builder.getResult();
-    }
-
-    private void populate(MapBuilder builder) {
-        // TODO: Add randomly generated monsters and objects to the map.
-
-        builder.buildMap();
+        return populate(builder);
     }
 
     public String[] getExistingMapNames() {
         return loader.getMapNames();
+    }
+
+    private Map populate(MapBuilder builder) {
+        addFixedObjects(builder);
+
+        builder.buildMap();
+        Map map = builder.getResult();
+
+        addWalkers(map);
+
+        return map;
+    }
+
+    private void addFixedObjects(MapBuilder builder) {
+
+    }
+
+    private void addWalkers(Map map) {
+        MonsterGenerator generator = new MonsterGenerator();
+
+        generator.setMinimumPerType(MINIMUM_MONSTER_PER_TYPE);
+
+        for (Walker walker : generator.generate(NUMBER_OF_MONSTERS))
+            map.add(walker);
     }
 }
