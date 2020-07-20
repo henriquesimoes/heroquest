323946da6c702e285929dd5526a0843d921e4c4a
Renan Fernando <renanfernando@localhost.localdomain>
2020-07-20 01:27:14 -0300

    Remove unnecessary lines in MagicMissile

--- src/br/unicamp/ic/mc322/heroquest/skills/magic/MagicMissile.java
+++ src/br/unicamp/ic/mc322/heroquest/skills/magic/MagicMissile.java
@@ -1,18 +1,17 @@
 package br.unicamp.ic.mc322.heroquest.skills.magic;
 
-import br.unicamp.ic.mc322.heroquest.map.core.AbstractMapObjectVisitor;
 import br.unicamp.ic.mc322.heroquest.map.core.MapObject;
 import br.unicamp.ic.mc322.heroquest.map.geom.Region;
 import br.unicamp.ic.mc322.heroquest.skills.MagicSkill;
 import br.unicamp.ic.mc322.heroquest.walker.Walker;
 
-public class MagicMissile extends MagicSkill implements AbstractMapObjectVisitor {
+public class MagicMissile extends MagicSkill {
     private static final int TOTAL_DAMAGE = 6;
 
     public MagicMissile() {
         super("Magic Missile",
                 "Three magic arrows are thrown against the target," +
-                        " with " + TOTAL_DAMAGE / 3 + " damage each");
+                        " with " + TOTAL_DAMAGE / 2 + " damage each");
     }
 
     @Override
@@ -26,7 +25,6 @@ public class MagicMissile extends MagicSkill implements AbstractMapObjectVisitor
 
     @Override
     public void updateTargets() {
-        // TODO: discover how to set the distance to catch the visible walkers
         Region region = getUserRegionSelector().getVisibleRegion(false);
         accept(this, region);
     }
