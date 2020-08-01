193db7eb83be1c1afff35ccf3c01f9b31b554100
Henrique Simões <henriquesimoespa@gmail.com>
2020-08-01 12:24:04 -0300

    Add argument to change game interface
    Now, the graphic interface is the default launching choice.
    If the command line interface is desired, the "--cli" option has
    to be given to the game start-up class.
    
    In order to make the command line parsing, the org.apache.commons
    for command line interface package is being used. In order to make
    the dependencies available to the code respository, all IntelliJ
    configuration files have been included.

--- src/br/unicamp/ic/mc322/heroquest/Main.java
+++ src/br/unicamp/ic/mc322/heroquest/Main.java
@@ -2,19 +2,37 @@ package br.unicamp.ic.mc322.heroquest;
 
 import br.unicamp.ic.mc322.heroquest.engine.GameEngine;
 import br.unicamp.ic.mc322.heroquest.engine.gui.GraphicEngine;
+import br.unicamp.ic.mc322.heroquest.engine.terminal.TerminalEngine;
+import org.apache.commons.cli.*;
 
 public class Main {
-
     public static void main(String[] args) {
-        /*
-         * In order to execute the graphic interface engine, change the
-         * `TerminalEngine` to a `GraphicEngine`.
-         */
-        GameEngine engine = new GraphicEngine();
+        Options options = loadOptions();
+        CommandLineParser parser = new DefaultParser();
 
-        /*
-         * Starts the configured game engine.
-         */
-        engine.run();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+            GameEngine engine;
+
+            if (cmd.hasOption("--cli"))
+                engine = new TerminalEngine();
+            else
+                engine = new GraphicEngine();
+
+            engine.run();
+        } catch (ParseException ex) {
+            System.err.println(ex.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("heroquest", options);
+        }
+    }
+
+    private static Options loadOptions() {
+        Options options = new Options();
+
+        Option option = new Option("c", "cli", false, "Use CLI game version");
+        options.addOption(option);
+
+        return options;
     }
 }
